6f803e729d0d15cd9219970f49fcc045497f6ca2
Ruz <kianruzelln@gmail.com>
2025-05-13 15:46:06 +0800

    updated gitignore and index

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ try{
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="refresh" content="900; url=logout.php">
-  <title>RTWPB3</title>
+  <title>RTWPB32</title>
   <link rel="icon" href="logo/logo.jpg" type="image/x-icon" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
